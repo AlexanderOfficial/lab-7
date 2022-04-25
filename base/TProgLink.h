b58affcb77874324b50f9a1c31c09ab8935685b6
AlexanderOfficial <a.krainev02@gmail.com>
2022-04-25 22:32:17 +0300

    add new classes

---
+++ base/TProgLink.h
@@ -0,0 +1,17 @@
+﻿//#pragma once
+//zveno
+#ifndef __INTERFACR_H__
+#define __INTERFACE_H__
+
+//#include "THashTableOnList.h"
+#include <conio.h>
+#include "TPostfix.h"
+
+//класс кредставляет собой звенья иерархического списка
+class TProgLink {
+protected:
+	static TProgLink MemHeader; 
+public:
+};
+
+#endif
